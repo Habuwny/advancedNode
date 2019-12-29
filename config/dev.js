12dbd57351b5cee2cb023a07dfee28bbd7fea579
AhmedHabony <52913323+AhmedHabony@users.noreply.github.com>
2019-12-29 18:20:30 +0200

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb+srv://ahmed_habony:gYXkUUKqdx0zUiSl@cluster0-tpdbz.mongodb.net/blog_dev?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+
+};
